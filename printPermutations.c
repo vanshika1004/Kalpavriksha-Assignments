d00ffc1a1ecf314cd854d8fac2d4a36df73457c7
vanshika1004 <116948370+vanshika1004@users.noreply.github.com>
2025-01-14 19:39:43 +0530

    Create printPermutations.c

---
+++ printPermutations.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <string.h> 
+#include<stdlib.h>
+
+void swap(char *x, char *y) 
+{ 
+	char temp; 
+	temp = *x; 
+	*x = *y; 
+	*y = temp; 
+} 
+
+void printPermutations(char *str, int index, int n) 
+{ 
+	if (index == n) 
+		printf("%s ", str); 
+	else
+	{ 
+		for (int j = index; j <= n; j++) 
+		{ 
+			swap((str + index), (str + j)); 
+			printPermutations(str, index + 1, n); 
+
+			//backtrack 
+			swap((str + index), (str + j)); 
+		} 
+	} 
+} 
+
+int main() 
+{ 
+	char* str = (char*)malloc(100 * sizeof(char));
+	scanf("%99[^\n]%*c",str);
+	int len = strlen(str); 
+	int index = 0;
+    printPermutations(str, index, len-1); 
+	return 0; 
+} 
